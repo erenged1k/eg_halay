3a4b7d359ea2d0ae60d5463e7b366596ac7e3bbf
Eren Gedik <erengedik911@gmail.com>
2020-09-20 18:35:41 +0300

    v1

---
+++ client.lua
@@ -0,0 +1,10 @@
+local connected = false
+
+RegisterCommand("halay",function()
+	ExecuteCommand("e uçak")
+	ExecuteCommand("e danszevzek")
+	end)
+
+RegisterCommand("halay2",function()
+	ExecuteCommand("e titanik")
+	end)
---
+++ readme.txt
@@ -0,0 +1,9 @@
+dpemotes ile çalışır - https://www.fivemturk.com/threads/turkce-dpemotes.4586/
+
+aranan halay scripti.
+
+Önizlemesi
+https://streamable.com/b4xc55
+
+erengedik.com
+
